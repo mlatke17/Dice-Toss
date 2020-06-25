c3e29dbc0e5b59dd4ff68e2a28fb3f7daf742e7c
mlatke17 <66775522+mlatke17@users.noreply.github.com>
2020-06-25 16:41:10 +0530

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,15 @@
+var randomNumber1 = Math.floor(Math.random()*6 + 1);
+var randomDice1 = "images/dice" + randomNumber1 + ".png";
+var randomNumber2 = Math.floor(Math.random()*6 + 1);
+var randomDice2 = "images/dice" + randomNumber2 + ".png";
+document.querySelectorAll("img")[0].setAttribute("src", randomDice1);
+document.querySelectorAll("img")[1].setAttribute("src", randomDice2);
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 1 wins😁";
+}
+else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = "Player 2 wins😁";
+}
+else{
+    document.querySelector("h1").innerHTML = "DRAW";
+}
